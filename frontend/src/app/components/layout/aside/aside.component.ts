ed3e48e35a2454a02e7b902576fcc662b70bcb33
daltahona <daltahona@uniguajira.edu.co>
2024-10-14 18:52:51 -0500

    Actualizacion

--- frontend/src/app/components/layout/aside/aside.component.ts
+++ frontend/src/app/components/layout/aside/aside.component.ts
@@ -13,59 +13,32 @@ import { Router } from '@angular/router';
 })
 export class AsideComponent implements OnInit {
   items: MenuItem[] | undefined;
-
   activeItem: MenuItem | undefined;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.items = [
-      {
-        label: 'Empleados',
-        icon: 'pi pi-users',
-        routerLink: '/empleados'
-      },
-      {
-        label: 'Proveedores',
-        icon: 'pi pi-building',
-        routerLink: '/proveedores'
-      },
-      {
-        label: 'Detalle Productos',
-        icon: 'pi pi-qrcode',
-        routerLink: '/detalle-productos'
-      },
-      {
-        label: 'Categorias',
-        icon: 'pi pi-tag',
-        routerLink: '/categorias'
-      },
-      {
-        label: 'Tipo Productos',
-        icon: 'pi pi-qrcode',
-        routerLink: '/tipo_Productos'
-      },
-      {
-        label: 'Productos',
-        icon: 'pi pi-shopping-bag',
-        routerLink: '/productos'
-      }
+      { label: 'Empleados', icon: 'pi pi-users', routerLink: '/empleados' },
+      { label: 'Proveedores', icon: 'pi pi-building', routerLink: '/proveedores' },
+      { label: 'Detalle Productos', icon: 'pi pi-qrcode', routerLink: '/detalle-productos' },
+      { label: 'Categorías', icon: 'pi pi-tag', routerLink: '/categorias' },
+      { label: 'Tipo Productos', icon: 'pi pi-qrcode', routerLink: '/tipo_Productos' },
+      { label: 'Productos', icon: 'pi pi-shopping-bag', routerLink: '/productos' }
     ];
-
-    this.activeItem = this.items && this.items[0];
+    this.activeItem = this.items[0]; // Establece el primer elemento activo por defecto
   }
 
   onActiveItemChange(event: any) {
     const menuItem = event.item;
-    this.activeItem = menuItem;
-    if (menuItem.routerLink) {
+    if (menuItem && menuItem.routerLink) {
+      this.activeItem = menuItem;
       this.router.navigate([menuItem.routerLink], { replaceUrl: true });
-      document.body.classList.add('animate');
-      setTimeout(() => {
-        console.log('Se eliminó la clase animate');
-        document.body.classList.remove('animate');
-      }, 500);
     }
   }
 }
+
+
+
+
 
